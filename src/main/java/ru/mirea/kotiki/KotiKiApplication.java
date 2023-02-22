9f9edcef7ad77fc8ddd0fc6b8c8820223477fefb
syasyayas <ya.sizkov@gmail.com>
2023-02-22 13:24:05 +0300

    initial commit

---
+++ src/main/java/ru/mirea/kotiki/KotiKiApplication.java
@@ -0,0 +1,13 @@
+package ru.mirea.kotiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotiKiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotiKiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mirea/kotiki/KotiKiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.kotiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotiKiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
