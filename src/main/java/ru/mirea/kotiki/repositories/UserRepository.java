90097e74155310fb21f71abeba4542ca9749d4f5
syasyayas <ya.sizkov@gmail.com>
2023-03-01 22:04:55 +0300

    feat: add UserRepository and UserDetailsService

--- src/main/java/ru/mirea/kotiki/repositories/UserRepository.java
+++ src/main/java/ru/mirea/kotiki/repositories/UserRepository.java
@@ -1,7 +1,11 @@
 package ru.mirea.kotiki.repositories;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 import ru.mirea.kotiki.domain.User;
-
+@Repository
 public interface UserRepository extends ReactiveCrudRepository<User, Long> {
+
+    public Mono<User> findByEmail(String email);
 }
---
+++ src/main/java/ru/mirea/kotiki/services/UserDetailsService.java
@@ -0,0 +1,23 @@
+package ru.mirea.kotiki.services;
+
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import ru.mirea.kotiki.repositories.UserRepository;
+
+@Service
+public class UserDetailsService implements ReactiveUserDetailsService {
+
+    UserRepository userRepo;
+
+    public UserDetailsService(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public Mono<UserDetails> findByUsername(String email) {
+        return userRepo.findByEmail(email).cast(UserDetails.class);
+
+    }
+}
