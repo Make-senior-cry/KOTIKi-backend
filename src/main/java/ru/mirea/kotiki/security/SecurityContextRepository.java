da51ac3ff21270a69627fb977caeb767befc186e
syasyayas <ya.sizkov@gmail.com>
2023-03-04 01:01:32 +0300

    feat: add AuthenticationManager and SecurityContextRepository

---
+++ src/main/java/ru/mirea/kotiki/security/SecurityContextRepository.java
@@ -0,0 +1,46 @@
+package ru.mirea.kotiki.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.web.server.context.ServerSecurityContextRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class SecurityContextRepository implements ServerSecurityContextRepository {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public SecurityContextRepository(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Mono<Void> save(ServerWebExchange exchange, SecurityContext context) {
+        throw new IllegalStateException("Save method not supported!");
+    }
+
+    @Override
+    public Mono<SecurityContext> load(ServerWebExchange exchange) {
+        String authHeader = exchange.getRequest()
+                .getHeaders()
+                .getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String authToken = authHeader.substring(7);
+
+            UsernamePasswordAuthenticationToken auth
+                    = new UsernamePasswordAuthenticationToken(authToken, authToken);
+            return authenticationManager
+                    .authenticate(auth).map(SecurityContextImpl::new);
+        }
+
+        return Mono.empty();
+    }
+}
