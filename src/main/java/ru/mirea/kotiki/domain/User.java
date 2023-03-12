330175b21daa96e189439896e16a0d73f51f5789
syasyayas <ya.sizkov@gmail.com>
2023-03-13 00:00:34 +0300

    fix: add some annotations and setters to work great with stream operations

--- src/main/java/ru/mirea/kotiki/domain/User.java
+++ src/main/java/ru/mirea/kotiki/domain/User.java
@@ -1,5 +1,6 @@
 package ru.mirea.kotiki.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -13,6 +14,7 @@ import java.util.Collection;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Table("usr")
 public class User implements UserDetails {
     @Id
@@ -32,6 +34,7 @@ public class User implements UserDetails {
     private UserRole role;
 
     private Timestamp creationTimestamp;
+
 
     public User(String name, String email, String password) {
         this.name = name;
--- src/main/java/ru/mirea/kotiki/dto/UserDto.java
+++ src/main/java/ru/mirea/kotiki/dto/UserDto.java
@@ -1,9 +1,14 @@
 package ru.mirea.kotiki.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.mirea.kotiki.domain.User;
 import ru.mirea.kotiki.domain.UserRole;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     private Long id;
     private String name;
@@ -14,4 +19,35 @@ public class UserDto {
     private Integer followersCount;
     private Integer followingCount;
 
+
+    public UserDto(User user, Integer followersCount, Integer followingCount) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.imageUrl = user.getImagePath();
+        this.description = user.getDescription();
+        this.email = user.getEmail();
+        this.role = user.getRole();
+        this.followingCount = followingCount;
+        this.followersCount = followersCount;
+    }
+
+    public UserDto(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.imageUrl = user.getImagePath();
+        this.description = user.getDescription();
+        this.email = user.getEmail();
+        this.role = user.getRole();
+
+    }
+
+    public UserDto setFollowersCount(Integer followersCount) {
+        this.followersCount = followersCount;
+        return this;
+    }
+
+    public UserDto setFollowingCount(Integer followingCount) {
+        this.followingCount = followingCount;
+        return this;
+    }
 }
