26c86dc8a569dc61e66b29c48b10644ad9ef0825
syasyayas <ya.sizkov@gmail.com>
2023-03-13 00:02:25 +0300

    feat: add UserController, UserService and endpoint to get user by id

---
+++ src/main/java/ru/mirea/kotiki/services/UserService.java
@@ -0,0 +1,32 @@
+package ru.mirea.kotiki.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import ru.mirea.kotiki.dto.UserDto;
+import ru.mirea.kotiki.repositories.UserRepository;
+
+@Service
+@Slf4j
+public class UserService {
+
+    private final UserRepository userRepo;
+
+    @Autowired
+    public UserService(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+
+    public Mono<UserDto> getUser(Integer id){
+        return userRepo.findById(id)
+                .switchIfEmpty(Mono.error(new Exception()))
+                .map(UserDto::new)
+                .flatMap(dto -> userRepo.getFollowingCountById(id).flatMap(i -> Mono.just(dto.setFollowingCount(i))))
+                .flatMap(dto -> userRepo.getFollowerCountById(id).flatMap(i -> Mono.just(dto.setFollowersCount(i))));
+
+    }
+
+
+}
