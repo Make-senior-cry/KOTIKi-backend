d63d6c2a4b84eb68311e53ce3eda47cbb1b351e5
syasyayas <ya.sizkov@gmail.com>
2023-03-13 14:48:12 +0300

    feat: add cookies to registration and return DTO

--- src/main/java/ru/mirea/kotiki/controllers/AuthController.java
+++ src/main/java/ru/mirea/kotiki/controllers/AuthController.java
@@ -2,22 +2,22 @@ package ru.mirea.kotiki.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebSession;
 import reactor.core.publisher.Mono;
 import ru.mirea.kotiki.domain.User;
+import ru.mirea.kotiki.dto.UserDto;
 import ru.mirea.kotiki.security.JwtUtil;
 import ru.mirea.kotiki.services.UserDetailsService;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -40,12 +40,15 @@ public class AuthController {
     }
 
     @PostMapping("/sign-up")
-    public Mono<ResponseEntity<Object>> register(@RequestBody User user) {
+    public Mono<ResponseEntity<UserDto>> register(ServerWebExchange swe, @RequestBody User user) {
         log.info("User registration with email \"" + user.getEmail() + "\" started");
         return Mono.just(user)
+                .cast(User.class)
                 .flatMap(u -> userDetailsService.register(user))
-                .flatMap(u -> Mono.just(OK))
-                .switchIfEmpty(Mono.just(BAD_REQUEST));
+                .doOnNext(u -> setCookies(swe.getResponse(), u))
+                .flatMap(u -> Mono.just(new UserDto(u)))
+                .flatMap(u -> Mono.just(ResponseEntity.ok(u)))
+                .defaultIfEmpty(ResponseEntity.badRequest().body(new UserDto()));
     }
 
     @PostMapping("/sign-in")
