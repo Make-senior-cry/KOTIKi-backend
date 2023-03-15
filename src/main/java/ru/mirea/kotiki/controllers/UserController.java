508d921e15b29787ec70abdb4bf415acf5fdfef5
syasyayas <ya.sizkov@gmail.com>
2023-03-16 00:00:52 +0300

    feat: add update user

--- src/main/java/ru/mirea/kotiki/controllers/UserController.java
+++ src/main/java/ru/mirea/kotiki/controllers/UserController.java
@@ -1,28 +1,50 @@
 package ru.mirea.kotiki.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 import ru.mirea.kotiki.dto.UserDto;
+import ru.mirea.kotiki.security.JwtUtil;
 import ru.mirea.kotiki.services.UserService;
 
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
 
     private final UserService userService;
+    private final JwtUtil jwtUtil;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JwtUtil jwtUtil) {
         this.userService = userService;
+
+        this.jwtUtil = jwtUtil;
     }
 
     @GetMapping
     public Mono<UserDto> getCount(@RequestParam Long id){
         return userService.getUser(id);
+    }
+
+    @PutMapping
+    public Mono<UserDto> updateUser(ServerWebExchange swe,
+                                    @RequestPart(required = false) String name,
+                                    @RequestPart(required = false) String description,
+                                    @RequestPart(required = false) Mono<FilePart> imageFile) {
+        return userService.updateUser(
+                jwtUtil
+                        .getClaimsFromAccessToken(swe
+                                .getRequest()
+                                .getCookies()
+                                .getFirst("access-token")
+                                .getValue())
+                        .getSubject()
+                ,name,description, imageFile);
+
     }
 }
--- src/main/java/ru/mirea/kotiki/services/UserService.java
+++ src/main/java/ru/mirea/kotiki/services/UserService.java
@@ -2,14 +2,22 @@ package ru.mirea.kotiki.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import ru.mirea.kotiki.domain.User;
 import ru.mirea.kotiki.dto.UserDto;
 import ru.mirea.kotiki.repositories.UserRepository;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Service
 @Slf4j
 public class UserService {
+
+    private final Path imagePath = Paths.get("./src/main/resources/images/user");
 
     private final UserRepository userRepo;
 
@@ -29,4 +37,28 @@ public class UserService {
     }
 
 
+    public Mono<UserDto> updateUser(String email, String name, String description, Mono<FilePart> image) {
+        Mono<User> user = userRepo.findByEmail(email);
+        AtomicReference<String> imageName = new AtomicReference<>();
+        image.flatMap(fp -> {
+            user.flatMap(u -> {
+                u.setImagePath(imagePath.toString() + "/upload/" + fp.filename());
+                return Mono.empty();
+            });
+            return fp.transferTo(imagePath.resolve("/upload/" + fp.filename()));
+        }).switchIfEmpty(Mono.empty());
+        if(name != null){
+            user.flatMap(u -> {
+                u.setName(name);
+                return Mono.just(u);
+            });
+        }
+        if(description != null){
+            user.flatMap(u -> {
+                u.setDescription(description);
+                return Mono.just(u);
+            });
+        }
+        return user.flatMap(userRepo::save).flatMap(u -> getUser(u.getId()));
+    }
 }
